e97dc3cabe8d2cfc22de6c2bb1a237757e9d0278
Abraham Barrera <abarrerac@gmail.com>
2017-09-05 23:55:19 -0300

    Todo es un objeto

---
+++ example.rb
@@ -0,0 +1,11 @@
+# Todo es un Objeto
+# Ruby permite agregar métodos y variables de instancia
+# a todos sus tipos de datos.
+100.zero?
+
+# String
+'Hello'.reverse
+
+# Todo Todo es un Objeto
+nil.nil?
+
