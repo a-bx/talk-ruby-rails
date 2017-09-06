9127f1c1c57a4c8915fbd7d87538cc7c525beb06
Abraham Barrera <abarrerac@gmail.com>
2017-09-06 00:21:56 -0300

    Jugando con Clases

--- example.rb
+++ example.rb
@@ -1,5 +1,5 @@
 # Todo es un Objeto
-# Ruby permite agregar métodos y variables de instancia
+# Ruby permite agregar metodos y variables de instancia
 # a todos sus tipos de datos.
 100.zero?
 
@@ -9,3 +9,24 @@
 # Todo Todo es un Objeto
 nil.nil?
 
+########################################################################
+
+# Definicion de la Clase
+class Persona
+  # metodo inicializar
+  def initialize(nombre = 'NONAME001')
+    @nombre = nombre
+  end
+
+  def hablar
+    puts "Hola soy #{@nombre}"
+  end
+end
+
+# Class es un Objeto 💥💥💥💥
+Hombre = Class.new Persona
+yo = Hombre.new
+yo.hablar
+# => Hola soy NONAME001
+Hombre.ancestors
+# => [Hombre, Persona, Object, Kernel, BasicObject]
