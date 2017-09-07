ec2611770bf27a30427e7d407b61ca2f29717fa5
Abraham Barrera <abarrerac@gmail.com>
2017-09-07 10:57:52 -0300

    Doc

--- example.rb
+++ example.rb
@@ -1,3 +1,12 @@
+=begin
+  Ruby es un lenguaje creado para la felicidad del Programador
+  Es un lenguaje
+    * Dinámico
+    * Interpretado
+    * Reflexivo
+    * Orientado a Objetos
+=end
+
 # Todo es un Objeto
 # Ruby permite agregar metodos y variables de instancia
 # a todos sus tipos de datos.
@@ -9,6 +18,8 @@
 # Todo Todo es un Objeto
 nil.nil?
 
+# Shortcuts para crear objetos:
+#
 # String	    '   "	      "mi string"
 # Simbolo	    :	          :symbol
 # Array	      [ ]	        [1, 2, 3, 4, 5]
@@ -20,28 +31,38 @@ nil.nil?
 
 # Definicion de la Clase
 class Persona
-  # metodo inicializar
-  def initialize(nombre = 'NONAME001')
-    @nombre = nombre
+  # metodo inicializar (equivalente al constructor)
+  def initialize(nombre = 'NONAME001') # pasando un argumento con valor default
+    @nombre = nombre # @ se utiliza para crear variables de instancia
   end
 
+  # los metodos de instancia se crean con def
   def hablar
+    # El objeto string permite interpolacion
+    # puts es equivalente a System.Out.print()
     puts "Hola soy #{@nombre}"
   end
 end
 
+# Los objetos se instancian con Clase.new(argumentos-del-constructor)
 pedro = Persona.new('Pedro')
+# En ruby no se necesitan parentesis para invocar un metodo
 pedro.hablar
 
-# Class es un Objeto 💥💥💥💥
-Hombre = Class.new Persona
+# Class es un Objeto y se pueden crear nuevas clases de esta forma
+Hombre = Class.new Persona # Hombre hereda de Persona
 yo = Hombre.new
 yo.hablar
 # => Hola soy NONAME001
+
+# Consultando los antecesores
 Hombre.ancestors
 # => [Hombre, Persona, Object, Kernel, BasicObject]
 
 # Interfaces?
+# No existen, en su defecto se utiliza el concepto
+# de duck typing:
+#   si habla como pato, si nada como pato, es un pato
 def conversar(person_one, person_two)
   person_one.hablar
   person_two.hablar
@@ -49,11 +70,14 @@ end
 
 juan = Hombre.new('Juan')
 pedro = Persona.new('Pedro')
+# Invocando el metodo conversar con 2 personas
 conversar juan, pedro
 
+# Si queremos verificar si un objeto responde a cierto mensaje (metodo)
 juan.respond_to? :hablar
 # => true
 
+# Si queremos verificar si un objeto es de una determinada Clase
 juan.instance_of? Persona
 # => false
 juan.instance_of? Hombre
@@ -61,16 +85,24 @@ juan.instance_of? Hombre
 
 # Definicion de la Clase
 class Chileno < Persona
+  # define getters y setters para el atributo id
   attr_accessor :id
+  # define un getter para el atributo nombre
   attr_reader :nombre
 
-  def self.new_id
+  # self dentro de la clase se utiliza para crear
+  # un metodo de clase accesible como Chileno.generate_id
+  # en otros lenguajes: static method
+  def self.generate_id
     rand 1000
   end
 
-  # metodo inicializar
   def initialize(nombre = nil)
-    self.id = Chileno.new_id
+    # se genera un id con el metodo de la clase
+    self.id = Chileno.generate_id
+    # se invoca a super (el consutrctor de la clase persona)
+    # si nombre es nulo, la evaluacion booleana
+    # pasara a ese constructor nombre = 'NONAME<ID>'
     super nombre || "NONAME#{id}"
   end
 end
@@ -80,14 +112,29 @@ daniel = Chileno.new('Daniel')
 puts daniel.id
 puts daniel.nombre
 
-# Permite jugar
+# MIXINS en vez de Herencia multiple
+# Los modulos permiten, entre otras cosas, definir comportamientos
+# Dentro de un modulo se pueden definir metodos y atributos
+# que finalmente seran incluidos en otras clases, es la forma que ruby
+# tiene para componer.
+
+# Modulo Jugador: Permite jugar
 module Jugador
+  # por convencion el signo de exclamacion al final
+  # nos indica que este mensaje cambiara al objeto
+  # no hay ninguna diferencia tecnica
   def jugar!
-    p 'Jugando'
+    p 'Jugando' # p es equivalente a puts
     @jugando = true
   end
 
+  # por convencion el signo de interrogacion al final
+  # nos indica que este mensaje devuelve boolenao
   def jugando?
+    # || es un OR; en este caso se evalua @jugando
+    # si @jugando es nil, se evalua false
+    # ruby devuelve siempre lo ultimo que evalua
+    # no es necesario hacer return
     @jugando || false
   end
 
@@ -98,67 +145,90 @@ end
 
 # Definicion de la Clase
 class Futbolista < Chileno
-  include Jugador
+  include Jugador # incluye el comportamiento de un jugador
 end
 
 alexis = Futbolista.new('Alexis')
 alexis.jugando?
 alexis.jugar!
+alexis.jugando?
 
+# Azucar sintactico
 class Estudiante < Chileno
+  # getter para atributo carrera
   attr_reader :carrera
 
+  # en este caso ambos argumentos son obligatorios para la construccion
   def initialize(nombre, carrera)
+    # se ejecuta el consutrctor de la clase Chileno que solo recibe nombre
     super nombre
+    # se asigna carrera
     @carrera = carrera
+  end
+
+  # se pueden definir metodos setters de esta forma
+  # utilizando el signo = como parte del nobmre del metodo
+  def carrera=(value)
+    @carrera = value
   end
 end
 
 juanito = Estudiante.new('Juanito', 'ing. en computacion')
 juanito.carrera
-# Escribe un setter
-
-class Estudiante < Chileno
-  attr_reader :carrera
-
-  def initialize(nombre, carrera)
-    super nombre
-    @carrera = carrera
-  end
-
-  def carrera=(value)
-    @carrera = value
-  end
-end
-juanito.carrera
-juanito.carrera=('Ing. Comercial')
-juanito.carrera =('Ing. Comercial')
-juanito.carrera = 'Ing. Comercial'
+# se invoca de esta manera el setter
+juanito.carrera=('nueva carrera')
+# El azucar sintactico de ruby nos permite ir simplificando est allamada
+# elimninar parentesis
+juanito.carrera= 'nueva carrera'
+# separar el caracter =
+juanito.carrera = 'nueva carrera'
 
 ###################################
 # Bloques
+# son Trozos de codigo que se envuelven
+# entre { .. } (singleline)
+# o     do .. end  (multiline)
 
 n = 10
+# el metodo times de la clase FixNum
+# recibe como parametro un bloque
+# y lo ejecuta n veces
 n.times do
   p "esto se repetirá #{n} veces"
 end
 
+# el metodo times ejecuta el bloque tambien pasandole argumentos
+# en este caso se puede ver que los argumentos de los bloques
+# se pueden recibir entre | argumentos.., .. |
+# para el caso de times es un unico argumento de tipo FixNum
+# que corresponde al index de la llamada
 5.times { |i| puts i }
 
+# La programacion funcional es bienvenida en Ruby
+# y se aprovechan de los bloques
 [1, 2, 3].reduce { |s, m| s + m }
 
 #################################
+# Ejemplo de utilizacion de bloques
+# un metodo times que ejecuta un bloque
+# pasado implicitamente
+# este metodo aparentemente no recibe ningun parametro
+# pero en ruby se pueden pasar implicitamente
 
 def times
-  result = yield(1, 100)
-  p result
+  if block_given? # metodo de kernel para verificar si viene un bloque
+    p yield(1, 100) # yield ejecuta el bloque que se paso por parametros
+  end
 end
 
+# aca ejecutamos a times pasandole un bloque
 times do |num_one, num_two|
   puts "#{num_one} #{num_two}"
   # operar los dos numeros
   num_one + num_two
 end
 
+# lambda crea Procs que es basicamente convertir un bloque en un objeto
 bloque = lambda { |i| puts i }
+# este bloque de codigo puede ser invocado posteriormente
 bloque.call 'parametro'
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+- En `example.rb` estan los ejemplos vistos en la charla
+
+- En `exampl/` hay un ejemplo de aplicación rails que ira mutando
+
+Slides:
+http://slides.com/abrahambarrera/ruby-rails
+
+- `respuesta.md` estarán las respuestas a sus preguntas
